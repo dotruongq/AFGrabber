91ac5b5d6a6a7c315699b7ab4fe90ae3458d9a08
dotruongq <36684308+dotruongq@users.noreply.github.com>
2022-10-10 22:14:57 -0700

    Update MainDebug.py
    Modified for personal usage

--- MainDebug.py
+++ MainDebug.py
@@ -9,12 +9,11 @@ import requests,json,time,uuid,sys,random,datetime
 
 
 headers = {
-    'x-amz-access-token': 'Atna|EwICIEfggdDMv_ZQ4dAPLt1_Tx3BYGOg__VrkJ5Rk3macH4wPicntda_E2zLeKsTVX3GQbe9q8uNjF2KyLwvnz_ulqpDPRbPWGxyGzyj0uqdvrGtjacqDwSL6QnxPdcfE1996sLh3qIJzQ1pLVzPTwsy2lkscwIpBevxkOX5NSgtAZPRc4xuSUVP6pHP1C6MfuwpR9yaslLW3p490itk1wEKlIvsgygpPetbKqAa72zjIXnTjP000WCAXeVeIiGPjHPANEB7vbYu6tuEGqSAX05Vadyp5NwZ68lF_CrPowSY5U_vugzSZ2tgYqH5iT_0-eIakEs3VzP6LrloGUzFjDSo4t49',
+    'x-amz-access-token': '',
 
 
 
-
-    'x-flex-instance-id': 'a3e8f85d-121a-46f3-abe8-ac9f026e74dc',
+    'x-flex-instance-id': '',
     
     
     'X-Amzn-RequestId': str(uuid.uuid4()),
@@ -153,14 +152,3 @@ while True: #3:27
     
     
     
-
-# https://flex-capacity-na.amazon.com/AcceptOffer
-
-    
-# {
-#   "__type": "AcceptOfferInput:http://internal.amazon.com/coral/com.amazon.omwbuseyservice.offers/",
-#   "offerId": "Ok9mZmVySWQuRW5jcnlwdGlvbktleS02aGlZbDQAAACDDtWCQwquLr5pJsGZl2myL4h3776OFJ8fwCyr4JUBvxT90Zb+McgTw1ReXrik3nNaxkr1rvzzP3b3PyA6KgD0pfDSC55Wrh5p4/pDRAHLSB5VfBw/Ttpg7LCARTeuhoJxD7kIapOP9GPAwui8xuglfoT63m3YHHrwm79V9STwR1eP2Wd5tZztArmG2xEiOgbkuH9JbIDP20EHoDjxz88QAAfftDNVR+q4v8ezpYgrog03eVl+NQq2zzJUmMMxVmOYnaNEmTHKHE4+lDHOHe9yT0vQNZV/Mc5sdqMo1XhstMH9SlY+NNvD9+0KR/zX4Dh4EeA4ezZWR9FwG+9PvhgyRfQaQCM0YAKPzZPGMXxFSVcI/mFgDyfTfNVPvahqCFqkxR4OVkzRFkpsbVIj6Cd5qYveAFfk75z6QtADA4XaJdPeFKybnhtRvj2IoZBYYjgnasxysZWT2HBEg40936ufH54pf6wmnAQoX79AJWUbv6+Ichk4+xsGu9rEeWrggFK+0hm5E75w5TN8fTACtIvFALeU3voIK6XW0eDaFoGnK9Wt/699/ZvGoC4R4nZAQeBXNErKVcsRfg==|mAeYxHSne8pbbXC2KUDfZFdFur4FwHVfBu4bZwL3Jq8="
-# }
-
-
-# {"offerExperienceVersion":"V1","offerList":[{"creationDate":null,"deliveryCutOffTimeEpoch":null,"deliveryRequest":{"deliveryRequestCount":1.0,"orderCount":10},"endTime":1.664658E9,"expirationDate":1.6646508E9,"hidden":false,"isPriorityOffer":false,"legalEntity":null,"maxWorkload":0,"offerId":"Ok9mZmVySWQuRW5jcnlwdGlvbktleS02aGlZbDQAAACDDtWCQwquLr4zKMfLwDy3f4hytb3ZFJ8fxS6r4Z0H6BTy0pb0Mc4TlwJeX7uk3nNaxkr1rvzzP3b3PyA6KgD0pfDSC55Wrh5p4/pDRAHLSB5VfBw/Ttpg7LCARTeuhoJxD7kIapOP92vAwui8xuglfYL63m3YHHrwm79V9STwR1eP2Wd5tZztALiC2UslbVfk439KP4DP2UMCoGOmnMcQUlbc5mcDFru8uczppYgr9Ag5dgZ3ZQ62yWNXmMMxCTWanaNMkmbKTEhixTDHS7p1TkvSNZV/Mc5sdqMo1XhstMH9SlY+NNvD9+0KR/zX4Dh4EeA4ezZWR9FwEeRZrxNWLYp5IFxDE2jvp+q5UwUhPmx8ikQUakTedt1Ns6c9DQ+qyEoOUx+DDhFobQQvuXwkr4SDU1G578CqEIZdVYrYediLQfybzU1T6DyLps0GZTUubs5z5JTG3yURiYk7iq7PGswpe/t2mw1/D+kUcjNJ46uLcks0+0hc7YGde2q30Qfi0x6+FrYm4zR9LWBSvoPKsjvV1S9RXd7ohJOd6DQndg==|Ldt68PjhRStpl6AfPNATs/VxKxAub750/Tl2PG5Bj9U=","offerMetadata":null,"offerType":"NON_EXCLUSIVE","rateInfo":{"PriceDetails":null,"currency":"USD","isSurge":false,"priceAmount":44.0,"pricingUXVersion":"V2","projectedTips":30.0,"surgeMultiplier":null},"schedulingType":"BLOCK","serviceAreaId":"88c3e32b-7c01-4510-88bf-7f43b5d175eb","serviceTypeId":"amzn1.flex.st.v1.PuyOplzlR1idvfPkv5138g","serviceTypeMetadata":{"modeOfTransportation":null,"nameClassification":"STANDARD"},"startTime":1.6646508E9,"startingLocation":{"address":{"address1":"","address2":null,"address3":null,"addressId":null,"city":null,"countryCode":null,"name":null,"phone":null,"postalCode":null,"state":null},"geocode":{"latitude":0.0,"longitude":0.0},"locationType":null,"startingLocationName":""},"status":"OFFERED","trIds":null}],"refreshInterval":1000,"refreshTimeout":1800000}
